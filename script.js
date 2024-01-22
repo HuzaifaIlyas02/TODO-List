958e890d9bdbdb32fcd9bee72c293d8ddacdf27a
HuzaifaIlyas02 <huzaifailyas02@outlook.com>
2024-01-22 21:52:36 +0500

    Add script.js file (addTask(), addEventListener())

---
+++ script.js
@@ -0,0 +1,29 @@
+const inputBox = document.getElementById("input-box");
+const listContainer = document.getElementById("list-container");
+
+
+function addTask(){
+    if(inputBox.value === ""){
+        alert("Please enter a task");
+    }
+    else {
+        let li = document.createElement("li");
+        li.innerHTML = inputBox.value;
+        listContainer.appendChild(li);
+        let span = document.createElement("span");
+        span.innerHTML = "X";
+        li.appendChild(span);
+    }
+    inputBox.value = "";
+}
+
+listContainer.addEventListener("click", function(e){
+    if(e.target.tagName === "LI"){
+        e.target.classList.toggle("checked");
+    }
+    else if(e.target.tagName === "SPAN"){
+        e.target.parentElement.remove();
+    }
+}, false);
+
+
